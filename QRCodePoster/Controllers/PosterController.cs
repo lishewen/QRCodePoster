5f2362219ad2e2dc5716e422ada7043f657cb2a3
算神 <lishewen@msn.com>
2020-02-24 10:46:42 +0800

    图片上传

--- QRCodePoster/Controllers/PosterController.cs
+++ QRCodePoster/Controllers/PosterController.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using QRCodePoster.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace QRCodePoster.Controllers
@@ -10,9 +14,11 @@ namespace QRCodePoster.Controllers
     public class PosterController : Controller
     {
         private readonly EFContext db;
-        public PosterController(EFContext context)
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public PosterController(EFContext context, IWebHostEnvironment hostEnvironment)
         {
             db = context;
+            _hostEnvironment = hostEnvironment;
         }
         /// <summary>
         /// 全局提示信息方法
@@ -30,6 +36,18 @@ namespace QRCodePoster.Controllers
         private void ShowErrMessage(string msg)
         {
             ViewData["ErrMessage"] = msg;
+        }
+
+        /// <summary>
+        /// 抛出Http异常
+        /// </summary>
+        /// <param name="msg"></param>
+        protected void ThrowHttpResponseException(string msg)
+        {
+            //throw new HttpResponseException(new HttpResponseMessage(HttpStatusCode.InternalServerError)
+            //{
+            //	Content = new StringContent(msg)
+            //});
         }
 
         public IActionResult Create()
@@ -53,6 +71,41 @@ namespace QRCodePoster.Controllers
                 ShowErrMessage("添加失败");
                 return View(model);
             }
+        }
+        [HttpPost]
+        public async Task<UploadFileResult> Upload(IFormFile file)
+        {
+            string[] allowExtName = { ".jpg", ".gif", ".png" };
+            if (file == null)
+                ThrowHttpResponseException("请选择上传文件");
+
+            var fullName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.ToString().Trim('"');
+            int i = fullName.LastIndexOf('.');
+            var extName = fullName.Substring(i);
+
+            if (!allowExtName.Contains(extName))
+                ThrowHttpResponseException("该文件类型不允许上传");
+
+            string fileName = Guid.NewGuid().ToString() + extName;
+            var filePath = _hostEnvironment.WebRootPath + @"\attachment\images\" + fileName;
+            //保存文件
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                var inputStream = file.OpenReadStream();
+                await inputStream.CopyToAsync(fileStream);
+            }
+            //await file.SaveAsAsync(filePath);
+
+            return new UploadFileResult
+            {
+                name = fullName,
+                ext = extName.Replace(".", ""),
+                filename = "images/" + fileName,
+                attachment = "images/" + fileName,
+                url = "http://" + Request.Host + "/attachment/images/" + fileName,
+                is_image = 1,
+                filesize = (int)file.Length
+            };
         }
     }
 }
---
+++ QRCodePoster/Models/UploadFileResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRCodePoster.Models
+{
+	public class UploadFileResult
+	{
+		public string name { get; set; }
+		public string ext { get; set; }
+		public string filename { get; set; }
+		public string attachment { get; set; }
+		public string url { get; set; }
+		public int is_image { get; set; }
+		public int filesize { get; set; }
+		public int width { get; set; }
+		public int height { get; set; }
+	}
+}
