730408d55114b948d7646562ca9565152cd36896
算神 <lishewen@msn.com>
2020-02-24 10:08:50 +0800

    样式修正

--- QRCodePoster/wwwroot/js/config.js
+++ QRCodePoster/wwwroot/js/config.js
@@ -2,38 +2,23 @@
 	baseUrl: '/js',
 	paths: {
 		'jquery': '/lib/jquery/dist/jquery.min',
-		'jquery.jplayer': '/lib/jplayer/dist/jplayer/jquery.jplayer.min',
-		'contextMenu': '/lib/jQuery-contextMenu/dist/jquery.contextMenu.min',
+		'contextMenu': '/lib/jquery-contextmenu/jquery.contextMenu.min',
 		'bootstrap': '/lib/bootstrap/dist/js/bootstrap.min',
-		'filestyle': '/lib/bootstrap-filestyle/src/bootstrap-filestyle.min',
 		'colorpicker': '/lib/spectrum/spectrum',
-		'css': '/lib/require-css/css.min',
-		'underscore': '/lib/underscore/underscore-min',
-		'webuploader': '/js/webuploader.min'
+		'css': '/lib/require-css/css.min'
 	},
 	shim: {
-		'jquery.jplayer': {
-			exports: "$",
-			deps: ['jquery']
-		},
 		'bootstrap': {
 			exports: "$",
 			deps: ['jquery']
 		},
-		'contextMenu':{
+		'contextMenu': {
 			exports: "$",
 			deps: ['jquery']
-		},
-		'filestyle': {
-			exports: '$',
-			deps: ['bootstrap']
 		},
 		'colorpicker': {
 			exports: '$',
 			deps: ['css!/lib/spectrum/spectrum.css']
-		},
-		'webuploader': {
-			deps: ['css!/css/webuploader.css']
 		}
 	}
 });
