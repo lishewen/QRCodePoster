3e3ea97ee8e6342cde19d5a58bcae5036dfc9bfa
算神 <lishewen@msn.com>
2020-02-24 10:19:22 +0800

    js依赖

--- QRCodePoster/wwwroot/js/config.js
+++ QRCodePoster/wwwroot/js/config.js
@@ -2,12 +2,18 @@
 	baseUrl: '/js',
 	paths: {
 		'jquery': '/lib/jquery/dist/jquery.min',
+		'jquery.jplayer': '/lib/jplayer/jplayer/jquery.jplayer.min',
 		'contextMenu': '/lib/jquery-contextmenu/jquery.contextMenu.min',
 		'bootstrap': '/lib/bootstrap/dist/js/bootstrap.min',
 		'colorpicker': '/lib/spectrum/spectrum',
-		'css': '/lib/require-css/css.min'
+		'css': '/lib/require-css/css.min',
+		'underscore': '/lib/underscore.js/underscore-min'
 	},
 	shim: {
+		'jquery.jplayer': {
+			exports: "$",
+			deps: ['jquery']
+		},
 		'bootstrap': {
 			exports: "$",
 			deps: ['jquery']
